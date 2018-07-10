1881798878022a719e04aa93c0b1841cf2c71f60
Grzegorz Futa <grzesiekfuta@gmail.com>
2018-07-10 15:25:18 +0200

    removed blank lines

--- Task.java
+++ Task.java
@@ -1,21 +1,17 @@
 package com.task;
 
-
 import nl.flotsam.xeger.Xeger;
 import org.apache.commons.validator.routines.EmailValidator;
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task {
-
     public static void main(String[] args) {
         String username, password, email, newEmail, tel, newTel;
 
         // wzorce przed kompilacją
         String userPattern = ".{3,}";
-
         String lower = "[a-z]{5,}";
         String upper = "[A-Z]";
         String digits = "[0-9]";
@@ -27,8 +23,6 @@ public class Task {
 
         // wzorce po kompilacji
         Pattern pUser = Pattern.compile(userPattern);
-
-
         Pattern pTel = Pattern.compile(telPattern);
 
         Xeger generator = new Xeger(passwordPattern);
@@ -36,34 +30,18 @@ public class Task {
 
         // Scanner
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("REJESTRACJA");
-
         System.out.println(provideUser);
         username = scanner.nextLine();
-
         Matcher userMatcher = pUser.matcher(username);
 
         if (userMatcher.find()){
             System.out.printf("Nazwa użytkownika: %s %n", username);
-
-
-
         }
         else {
-
-                System.err.println("Podałeś za krótką nazwę użytkownika");
-                
-
+                System.err.println("Podałeś za krótką nazwę użytkownika");          
         }
-
-
-
-
-
         System.out.printf("Wygenerowane hasło: %s %n", password);
-
-
         System.out.println("Podaj adres email");
         email = scanner.nextLine();
 
@@ -73,15 +51,12 @@ public class Task {
         } else {
             System.err.println("Niepoprawny email");
         }
-
-
         System.out.println("Podaj nr telefonu");
         tel = scanner.nextLine();
         Matcher telMatcher = pTel.matcher(tel);
 
         if (telMatcher.find()) {
             System.out.printf("Nr telefonu: %s %n", tel);
-
         } else {
             System.err.println("Niepoprawny nr telefonu");
         }
@@ -99,7 +74,5 @@ public class Task {
         System.out.println("Zmiana nr telefonu");
         newTel = scanner.nextLine();
         System.out.printf("Nowy nr telefonu: %s %n", newTel);
-
-
     }
 }
